3ab6ba85011212be57e5cf57e9af305801314124
nicorojasv <n.rojas.valdes@gmail.com>
2025-04-18 21:03:24 -0400

    cambio en test

--- conftest.py
+++ conftest.py
@@ -5,5 +5,5 @@ from django.conf import settings
 
 def pytest_configure():
     sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-    settings.DEBUG = False
-    django.setup() 
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SMA.settings')
+    django.setup()
