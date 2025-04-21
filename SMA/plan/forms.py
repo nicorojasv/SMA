8303178fd878fc2ab0b056ab088d193acf06e3b8
Luis <luis.paillan.cnc@gmail.com>
2025-04-21 13:20:11 -0400

    Docstrings faltantes en admin, forms, models, permissions, y utils

--- SMA/plan/forms.py
+++ SMA/plan/forms.py
@@ -3,6 +3,23 @@ from django.contrib.auth.forms import UserCreationForm
 from plan.models import CustomUser, OrganismoSectorial
 
 class CustomUserCreationForm(UserCreationForm):
+    """
+    Formulario personalizado para la creación de usuarios, extendiendo 
+    el formulario estándar de Django `UserCreationForm`.
+
+    Este formulario incluye campos adicionales como teléfono, RUT y 
+    la asociación opcional a un organismo sectorial.
+
+    Atributos:
+        telefono (CharField): Campo obligatorio para ingresar el número de teléfono del usuario.
+        rut (CharField): Campo obligatorio para ingresar el RUT del usuario.
+        id_organismo_sectorial (ModelChoiceField): Campo opcional para seleccionar un organismo sectorial.
+    
+    Meta:
+        model (CustomUser): Modelo personalizado de usuario.
+        fields (list): Lista de campos a mostrar en el formulario.
+        widgets (dict): Personalización de widgets para los campos de contraseña.
+    """
     telefono = forms.CharField(max_length=15, required=True, label="Teléfono")
     rut = forms.CharField(max_length=12, required=True, label="RUT")
     id_organismo_sectorial = forms.ModelChoiceField(
--- SMA/plan/permissions.py
+++ SMA/plan/permissions.py
@@ -1,13 +1,57 @@
 from rest_framework.permissions import BasePermission
 
 class EsAdministrador(BasePermission):
+    """
+    Permiso para verificar si el usuario pertenece al grupo 'Administrador'.
+
+    """
     def has_permission(self, request, view):
+        """
+        Determina si el usuario tiene permiso para acceder a la vista especificada.
+
+        Args:
+            - request (HttpRequest): La solicitud HTTP actual.
+            - view (APIView): La vista a la que se está intentando acceder.
+        
+        Returns:
+            - bool: True si el usuario está autenticado y pertenece al grupo 'Administrador',
+                  False en caso contrario.
+        """
         return request.user and request.user.groups.filter(name='Administrador').exists()
 
 class EsFiscalizador(BasePermission):
+    """
+    Permiso para verificar si el usuario pertenece al grupo 'Fiscalizador'.
+
+    """
     def has_permission(self, request, view):
+        """
+        Determina si el usuario tiene permiso para acceder a la vista especificada.
+
+        Args:
+            - request (HttpRequest): La solicitud HTTP actual.
+            - view (APIView): La vista a la que se está intentando acceder.    
+        Returns:
+            - bool: True si el usuario está autenticado y pertenece al grupo 'Fiscalizador',
+                  False en caso contrario. 
+        """
         return request.user and request.user.groups.filter(name='Fiscalizador').exists()
 
 class EsOrganismoSectorial(BasePermission):
+    """
+    Permiso para verificar si el usuario pertenece al grupo 'Organismo sectorial'.
+    
+    """
     def has_permission(self, request, view):
+        """
+        Determina si el usuario tiene permiso para acceder a la vista especificada.
+
+        Args:
+            - request (HttpRequest): La solicitud HTTP actual.
+            - view (APIView): La vista a la que se está intentando acceder.
+        
+        Returns:
+            - bool: True si el usuario está autenticado y pertenece al grupo 'Organismo sectorial',
+                  False en caso contrario.
+        """
         return request.user and request.user.groups.filter(name='Organismo sectorial').exists()
