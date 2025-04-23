cda187b0b8893260c2ab181d3a6a8004f6496c50
Carlos Azocar Riquelme <78894939+Carlos-Azocar-Riquelme@users.noreply.github.com>
2025-04-23 00:23:26 -0400

    Configura la conexión a la base de datos para deshabilitar SSL y actualiza settings.py para incluir la opción de sslmode desde las variables de entorno.

--- SMA/SMA/settings.py
+++ SMA/SMA/settings.py
@@ -101,7 +101,11 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': os.getenv('DB_SSLMODE', 'require'),  # por defecto 'require'
+        },
     }
+
 }
 #DATABASES = {
 #    'default': {
